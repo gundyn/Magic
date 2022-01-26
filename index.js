5856cbc1d9502e8becb1db1292528e7c80bb2820
gundyn <nathan.gundy@gmail.com>
2022-01-26 11:28:08 -0600

    fetched data working on displaying what i want

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 'use strict'
 
 // Get All Cards
-    const api = 'https://api.magicthegathering.io/v1';
-    fetch(`${api}/cards`).then(res => res.json())
-    .then(json => console.log(json));
+  const api = 'https://api.magicthegathering.io/v1';
+  // fetch(`${api}/cards`).then(res => res.json())
+  // .then(json => console.log(json))
+  
+  async function getApi(api) {
+    const response = await fetch(`${api}/cards`)
+    
+    let data = await response.json()
+    console.log(data)
+    if (response) {
+      console.log('it worked?')
+    }
+    show(data)
+  }
+  
+  getApi(api)
+    
+  function show(data) {
+    console.log(data)
+    let tab = 
+      `<tr>
+        <th>id</th>
+      </tr>`
+      
+    for (let i of data.cards) {
+      tab += `<tr>
+                <td> ${i.name} </td>
+              </tr>`
+    }
+    document.getElementById('message').innerHTML = tab
+  }
+   
+  
